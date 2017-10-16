1a1c1dc36a355bac05fd55a209bc3270b5cfbae9
Mathieu Delestre <ldbglobe@gmail.com>
2017-10-16 13:44:00 +0200

    Repository init
    [WIP]

---
+++ src/ldbglobe/Tophat/Builder/Html.php
@@ -0,0 +1,11 @@
+<?php
+namespace ldbglobe\Tophat\Builder;
+
+class Html
+{
+	public function __construct($tophat)
+	{
+		print_r($tophat->getAll());
+	}
+}
+?>
---
+++ src/ldbglobe/Tophat/Tophat.php
@@ -0,0 +1,100 @@
+<?php
+// See test/index.php for usage
+
+namespace ldbglobe\Tophat;
+
+class Tophat {
+
+	private $settings = null;
+
+	public function __construct($initial_settings=null)
+	{
+		//https://packagist.org/packages/dflydev/dot-access-data
+		$this->settings = new \Dflydev\DotAccessData\Data($initial_settings);
+		// get
+		// getData (return a Data interface)
+		// set
+		// append (add entry to a unindexed data array)
+		// remove (remove a node)
+		// has (return true if node exists)
+		// import
+		// importData
+		// export
+	}
+
+	public function buildHtml()
+	{
+		return new \ldbglobe\Tophat\Builder\Html($this);
+	}
+
+	// ----------------------------------------
+	// Generic accessors
+	private function set($k,$v)
+	{
+		$this->settings->set($k, $v);
+	}
+	private function append($k,$v)
+	{
+		$this->settings->append($k, $v);
+	}
+	public function get($k)
+	{
+		return $this->settings->get($k);
+	}
+	public function getAll()
+	{
+		return $this->settings->export();
+	}
+
+	// ----------------------------------------
+	// LOGO
+	public function setLogo($v)
+	{
+		$this->settings->set('logo', $v);
+	}
+
+	// ----------------------------------------
+	// MODULES
+	public function setModule($k,$v)
+	{
+		$this->settings->set('modules.'.$k, $v);
+	}
+	public function getModule($k)
+	{
+		$this->settings->get('modules.'.$k);
+	}
+	public function appendModule($k,$v)
+	{
+		$this->settings->append('modules.'.$k, $v);
+	}
+
+	// ----------------------------------------
+	// BARS
+	public function setBar($k,$v)
+	{
+		$this->settings->set('bars.'.$k,$v);
+	}
+	public function getBar($k)
+	{
+		$this->settings->get('bars.'.$k);
+	}
+	public function appendBar($k,$v)
+	{
+		$this->settings->append('bars.'.$k, $v);
+	}
+
+	// ----------------------------------------
+	// NAVS
+	public function setNav($k,$v)
+	{
+		$this->settings->set('navs.'.$k,$v);
+	}
+	public function getNav($k)
+	{
+		$this->settings->get('navs.'.$k);
+	}
+	public function appendNav($k,$v)
+	{
+		$this->settings->append('navs.'.$k, $v);
+	}
+}
